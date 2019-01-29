42820032c017a8247fdc0774b56bcb61a2255836
ANUJ <anujsharma.as98@gmail.com>
2019-01-29 21:40:48 +0530

    My First Project

---
+++ app/src/main/java/com/anuj/echo/adapters/MainScreenAdapter.kt
@@ -0,0 +1,83 @@
+package com.anuj.echo.adapters
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.FragmentActivity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import com.anuj.echo.R
+import com.anuj.echo.Songs
+import com.anuj.echo.activities.MainActivity
+import com.anuj.echo.fragments.MainScreenFragment
+import com.anuj.echo.fragments.SongPlayingFragment
+import kotlinx.android.synthetic.main.row_custom_mainscreen_adapter.view.*
+
+class MainScreenAdapter(_songDetails: ArrayList<Songs>, _context: Context) :
+        RecyclerView.Adapter<MainScreenAdapter.MyViewHolder>() {
+
+    var songDetails: ArrayList<Songs>? = null
+    var mContext: Context? = null
+
+    init {
+        this.songDetails = _songDetails
+        this.mContext = _context
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder?, position: Int) {
+        val songObject = songDetails?.get(position)
+        holder?.trackTitle?.text = songObject?.songTitle
+        holder?.trackArtist?.text = songObject?.artist
+        holder?.contentHolder?.setOnClickListener({
+            val songPlayingFragment = SongPlayingFragment()
+            var args = Bundle()
+            args.putString("songArtist", songObject?.artist)
+            args.putString("path", songObject?.songData)
+            args.putString("songTitle", songObject?.songTitle)
+            args.putInt("SongId", songObject?.songID?.toInt() as Int)
+            args.putInt("songPosition", position)
+            args.putParcelableArrayList("songData", songDetails)
+            songPlayingFragment.arguments = args
+
+            (mContext as FragmentActivity).supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.details_fragment, songPlayingFragment)
+                    .addToBackStack("SongPlayingFragment")
+                    .commit()
+        })
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent?.context)
+                .inflate(R.layout.row_custom_mainscreen_adapter, parent, false)
+
+        return MyViewHolder(itemView)
+
+    }
+
+    override fun getItemCount(): Int {
+        if (songDetails == null) {
+            return 0
+        } else {
+            return (songDetails as ArrayList<Songs>).size
+        }
+
+    }
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var trackTitle: TextView? = null
+        var trackArtist: TextView? = null
+        var contentHolder: RelativeLayout? = null
+
+        init {
+            trackTitle = view.findViewById<TextView>(R.id.trackTitle)
+            trackArtist = view.findViewById<TextView>(R.id.trackArtist)
+            contentHolder = view.findViewById<RelativeLayout>(R.id.contentRow)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/anuj/echo/fragments/AboutUsFragment.kt
@@ -0,0 +1,66 @@
+package com.anuj.echo.fragments
+
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.anuj.echo.R
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class AboutUsFragment : Fragment() {
+
+    var myActivity: Activity? = null
+    var aboutUs: ImageView? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_about_us, container, false)
+        aboutUs = view?.findViewById(R.id.about_us)
+        activity.title = "About us"
+
+        return view
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        myActivity = context as Activity
+    }
+
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+        myActivity = activity
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?) {
+        super.onPrepareOptionsMenu(menu)
+        val item = menu?.findItem(R.id.action_sort)
+        item?.isVisible = false
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+
+}
